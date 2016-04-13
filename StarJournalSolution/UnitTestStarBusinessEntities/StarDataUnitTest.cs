3cfe68ea6046fdb0535a8df44c0a6760bb8102fa
WinApp ExamRef70484 <examref70484@outlook.com>
2016-04-13 16:28:10 +0200

    First StarData business entity with first unittest.

---
+++ StarJournalSolution/UnitTestStarBusinessEntities/StarDataUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using nl.NeoRenaissance.StarJournal.StarBusinessEntities;
+
+namespace nl.NeoRenaissance.StarJournal.UnitTestStarBusinessEntities {
+    [TestClass]
+    public class StarDataUnitTest {
+        [TestMethod]
+        public void TestCreatingStarObject_NewWorks() {
+            // arrange, act
+            var data = new StarData();
+            // assert
+            Assert.IsNotNull(data);
+        }
+    }
+}
